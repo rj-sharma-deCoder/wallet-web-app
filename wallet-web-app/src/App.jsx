ec34ad58fb0c09051f184f6a5b1618415102f81e
RJ Sharma <rohithjsharma28@.com>
2023-03-04 23:08:11 +0530

    initialized components

--- wallet-web-app/src/App.jsx
+++ wallet-web-app/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+//Import Components
+import Navbar from './components/home/Navbar'
+import BalanceCard from './components/home/BalanceCard'
+import Dashboard from './components/home/Dashboard'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Wallet</h1>
     </div>
   )
 }
---
+++ wallet-web-app/src/components/home/BalanceCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BalanceCard = () => {
+  return (
+    <div>BalanceCard</div>
+  )
+}
+
+export default BalanceCard
